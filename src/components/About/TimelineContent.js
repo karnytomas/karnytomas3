d2250d43163fc57ce7afdb59ed26285c5fb53aab
Vaše jméno <vas-email@example.com>
2024-11-20 18:13:29 +0100

    haha

--- src/components/About/TimelineContent.js
+++ src/components/About/TimelineContent.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function TimelineContent({image, title, description}){
     return(
-        <div className="flex flex-col gap-8 TimelineContent">
+        <div className="flex flex-col gap-4 lg:gap-8 TimelineContent">
             <img src={`./images/timeline/${image}`} className="w-full h-full rounded-lg"></img>
             <div>
-                <h3 className="mb-4 text-3xl">{title}</h3>
+                <h3 className="mb-2 lg:mb-4 text-3xl">{title}</h3>
                 <p className="text-sm font-normal">{description}</p>
             </div>
         </div>
