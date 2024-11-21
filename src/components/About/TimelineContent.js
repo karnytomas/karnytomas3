4ba47ee02c810cee668d0b7fa560eb50d3c1c5a4
Vaše jméno <vas-email@example.com>
2024-11-21 09:14:42 +0100

    text

--- src/components/About/TimelineContent.js
+++ src/components/About/TimelineContent.js
@@ -5,8 +5,8 @@ function TimelineContent({image, title, description}){
         <div className="flex flex-col gap-2 TimelineContent">
             <img src={`./images/timeline/${image}`} className="w-full h-full rounded-lg"></img>
             <div>
-                <h3 className="mb-2 text-3xl">{title}</h3>
-                <p className="text-sm font-normal">{description}</p>
+                <h3 className="mb-2 text-3xl xl:text-4xl 2xl:text-6xl">{title}</h3>
+                <p className="text-sm xl:text-lg 2xl:text-xl font-normal">{description}</p>
             </div>
         </div>
     );
