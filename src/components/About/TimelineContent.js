b3fd74bf50a7a9a4c43f27bab8749a1c72b01b21
Vaše jméno <vas-email@example.com>
2024-11-21 09:15:54 +0100

    Update TimelineContent.js

--- src/components/About/TimelineContent.js
+++ src/components/About/TimelineContent.js
@@ -6,7 +6,7 @@ function TimelineContent({image, title, description}){
             <img src={`./images/timeline/${image}`} className="w-full h-full rounded-lg"></img>
             <div>
                 <h3 className="mb-2 text-3xl xl:text-4xl 2xl:text-6xl">{title}</h3>
-                <p className="text-sm xl:text-lg 2xl:text-xl font-normal">{description}</p>
+                <p className="text-sm 2xl:text-lg font-normal">{description}</p>
             </div>
         </div>
     );
